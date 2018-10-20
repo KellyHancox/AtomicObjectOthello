7434316185375060e8b8b351d67d9db30f35b49d
Julia <hildejul.hildebrand@gmail.com>
2018-10-20 12:50:27 -0400

    first

--- sdks/java/AI/src/com/atomicobject/othello/AI.java
+++ sdks/java/AI/src/com/atomicobject/othello/AI.java
@@ -4,18 +4,24 @@ import java.util.Arrays;
 import java.util.ListIterator;
 
 public class AI {
-	
-	ListIterator<int[]> moveList;
 
-	public AI(int[][] moves) {
-		moveList = Arrays.asList(moves).listIterator();
-	}
+    ListIterator<int[]> moveList;
+    private int[] pointList;
 
-	public int[] computeMove(GameState state) {
-		System.out.println("AI returning canned move for game state - " + state);
-		return moveList.next();
-	}
+    public AI(int[][] moves) {
+        moveList = Arrays.asList(moves).listIterator();
+    }
 
-	
+    public int[] computeMove(GameState state) {
+        System.out.println("AI returning canned move for game state - " + state);
+        //return moving[0];
+        //return moveList.next();
+
+        return new int[]{4, 2};
+    }
+
+    public void findMove() {
+		
+    }
 
 }
